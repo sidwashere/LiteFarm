e6becbe32c989268cea1058ac2cda69c1e1231f6
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-03-03 15:42:12 -0800

    mock i18n

--- packages/webapp/src/setupTests.js
+++ packages/webapp/src/setupTests.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { jest } from '@jest/globals';
+
+jest.mock('./locales/i18n', () => ({
+  language: 'en',
+  t: (t) => t,
+}));
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (text) => text,
+  }),
+}));
